07fe1ce3a52d0318516a8c3a824f744dee1c8a72
Fabiano <fabiano.one@gmail.com>
2022-12-11 23:07:20 -0300

    refactor: add product routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const sequelize = require('./config/database')
-
+const productRoutes = require('./routes/product.routes')
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Hello API'})
-})
+app.use('/api/products', productRoutes)
 
 app.listen(3333, () => {
   console.log(`Server started on port 3333 🔥`);
