a2f82266ab2cc928cb817fd9b63c2c9abccf43f4
Fabiano <fabiano.one@gmail.com>
2022-12-11 23:28:51 -0300

    feat: add show one product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -18,5 +18,18 @@ module.exports = {
       console.log(error)
       res.status(400).send(error)
     }
+  },
+  async one(req, res) {
+    try {
+      const { id } = req.params
+      const product = await Product.findOne({ where: { id } })
+      if (!product) {
+        return res.status(400).json('Product not found')
+      }
+      res.status(200).json(product)
+    } catch (error) {
+      console.log(error)
+      res.status(400).send(error)
+    }
   }
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,5 +3,6 @@ const productController = require('../controllers/productController')
 const router = express.Router()
 
 router.route('/').get(productController.all).post(productController.create)
+router.route('/:id').get(productController.one)
 
 module.exports = router
