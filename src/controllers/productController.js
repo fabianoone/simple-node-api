05100e3fe0085146dead6e5c62202b6a1860a78f
Fabiano <fabiano.one@gmail.com>
2022-12-11 23:24:01 -0300

    feat: create a new product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,7 +6,16 @@ module.exports = {
       const products = await Product.findAll();
       res.status(200).json(products)
     } catch (error) {
-      console.log(error);
+      console.log(error)
+      res.status(400).send(error)
+    }
+  },
+  async create(req, res) {
+    try {
+      await Product.create(req.body)
+      res.status(200).json('product inserted!')
+    } catch (error) {
+      console.log(error)
       res.status(400).send(error)
     }
   }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const productController = require('../controllers/productController')
 const router = express.Router()
 
-router.route('/').get(productController.all)
+router.route('/').get(productController.all).post(productController.create)
 
 module.exports = router
